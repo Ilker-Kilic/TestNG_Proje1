42ad438d4d49a891af1b05c632385b8ba693feb9
ozgurrkarakus <ozgurkarakus.fb@gmail.com>
2024-03-28 22:30:42 +0300

    TC_404_407 - Solved

--- src/test/java/US_407/TC_407.java
+++ src/test/java/US_407/TC_407.java
@@ -1,4 +1,43 @@
 package US_407;
 
-public class TC_407 {
+import US_404.US_404_US_406_Elements;
+import Utility.BaseDriver;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+public class TC_407 extends BaseDriver {
+    @Test
+
+    public void PatientDeletion() throws AWTException {
+        US_404_US_406_Elements elements = new US_404_US_406_Elements();
+
+        Robot robot = new Robot();
+
+        for (int i = 0; i < 4; i++) {
+            robot.keyPress(KeyEvent.VK_TAB);
+            robot.keyRelease(KeyEvent.VK_TAB);
+        }
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_ENTER);
+
+
+        elements.formControl.sendKeys("William" + Keys.ENTER);
+
+        elements.deleteName.click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(elements.delete));
+        elements.delete.click();
+
+
+        elements.reason.sendKeys("Death");
+        elements.submitReason.click();
+        Assert.assertFalse(elements.alert2.getText().equals("William"), "Search Not Found");
+
+    }
 }
+
