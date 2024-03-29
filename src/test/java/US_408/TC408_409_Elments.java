6c04f544e841f700a44d48e60c5115f220a2d3d7
ali <alicmrt648@gmail.com>
2024-03-29 16:46:05 +0300

    TC_409 COMPLETED

--- src/test/java/US_408/TC408_409_Elments.java
+++ src/test/java/US_408/TC408_409_Elments.java
@@ -20,6 +20,30 @@ public class TC408_409_Elments {
     public WebElement dataTableInfo;
     @FindBy(xpath = "//*[@id='patient-search-results-table_next']")
     public WebElement dataTableNext;
+    @FindBy(xpath = "/html/body/header/nav/div[1]/a")
+    public WebElement goHomePage;
+    @FindBy(xpath = "//*[@id=\"content\"]/div[6]/div[2]/div")
+    public WebElement getID;
 
+    @FindBy(xpath = "//*[@id=\"coreapps-datamanagement-homepageLink-coreapps-datamanagement-homepageLink-extension\"]")
+    public WebElement DataManagement;
+
+    @FindBy(xpath = "//*[@id=\"coreapps-mergePatientsHomepageLink-app\"]")
+    public WebElement MergePatients;
+
+    @FindBy(xpath = "//*[@id=\"patient1-text\"]")
+    public WebElement Patient1;
+
+    @FindBy(xpath = "//*[@id=\"patient2-text\"]")
+    public WebElement Patient2;
+
+    @FindBy(xpath = "//*[@id=\"confirm-button\"]")
+    public WebElement mergeConfirm;
+
+    @FindBy(xpath ="//*[@id=\"second-patient\"]")
+    public WebElement selectPatient;
+
+    @FindBy(xpath = "//*[@id=\"content\"]/div[6]/div[2]/div")
+    public WebElement isMerge;
 
 }
--- src/test/java/US_409/TC_409.java
+++ src/test/java/US_409/TC_409.java
@@ -1,4 +1,159 @@
 package US_409;
 
-public class TC_409 {
+import US_404.US_404_US_406_Elements;
+import US_408.TC408_409_Elments;
+import Utility.BaseDriver;
+import Utility.Tools;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+public class TC_409 extends BaseDriver {
+    private String ID1;
+    private String ID2;
+    @Test
+    public void PatientRegistration() throws AWTException {
+        US_404_US_406_Elements elements = new US_404_US_406_Elements();
+        TC408_409_Elments myElements = new TC408_409_Elments();
+
+        LoginTest();
+        Robot robot = new Robot();
+
+        for (int i = 0; i < 7; i++) {
+            robot.keyPress(KeyEvent.VK_TAB);
+            robot.keyRelease(KeyEvent.VK_TAB);
+        }
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_ENTER);
+
+        elements.name.sendKeys("Ali");
+        elements.familyName.sendKeys("Cömert");
+        elements.next.click();
+        elements.gender.click();
+        Select genderValue = new Select(elements.gender);
+        genderValue.selectByValue("M");
+        elements.next.click();
+
+        elements.birthdateDay.sendKeys("15");
+        elements.birthdateMonth.click();
+        Select monthValue = new Select(elements.birthdateMonth);
+        monthValue.selectByValue("5");
+        elements.birthdateYear.sendKeys("2005");
+        elements.next.click();
+
+        elements.address1.sendKeys("Ata Mahallesi");
+        elements.address2.sendKeys("352. Sokak");
+        elements.cityVillage.sendKeys("Esenyurt");
+        elements.stateProvince.sendKeys("İstanbul");
+        elements.country.sendKeys("Türkiye");
+        elements.postalCode.sendKeys("34582");
+        elements.next.click();
+
+        elements.phoneNumber.sendKeys("050515154855");
+        elements.next.click();
+
+        elements.relationship_type.click();
+        Select relationship = new Select(elements.relationship_type);
+        relationship.selectByIndex(5);
+        elements.person_Name.sendKeys("Unknown");
+        elements.next.click();
+
+        elements.submit.click();
+        Tools.Bekle(5);
+        System.out.println("Soyad Kontrol Ediliyor... : "+elements.alert1.getText());
+        Assert. assertTrue(elements.alert1.getText().contains("Cömert"));
+        String IDFull = myElements.getID.getText();
+        ID1 = IDFull.substring(10);
+        System.out.println("ID = " +ID1 );
+
+    }
+    @Test
+    public void PatientRegistration2() throws AWTException {
+        US_404_US_406_Elements elements = new US_404_US_406_Elements();
+        TC408_409_Elments myElements = new TC408_409_Elments();
+        Robot robot = new Robot();
+        myElements.goHomePage.click();
+        for (int i = 0; i < 7; i++) {
+            robot.keyPress(KeyEvent.VK_TAB);
+            robot.keyRelease(KeyEvent.VK_TAB);
+        }
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_ENTER);
+
+        elements.name.sendKeys("Gencer");
+        elements.familyName.sendKeys("Genç");
+        elements.next.click();
+        elements.gender.click();
+        Select genderValue = new Select(elements.gender);
+        genderValue.selectByValue("M");
+        elements.next.click();
+
+        elements.birthdateDay.sendKeys("15");
+        elements.birthdateMonth.click();
+        Select monthValue = new Select(elements.birthdateMonth);
+        monthValue.selectByValue("5");
+        elements.birthdateYear.sendKeys("2005");
+        elements.next.click();
+
+
+
+        elements.address1.sendKeys("Ata Mahallesi");
+        elements.address2.sendKeys("352. Sokak");
+        elements.cityVillage.sendKeys("Esenyurt");
+        elements.stateProvince.sendKeys("İstanbul");
+        elements.country.sendKeys("Türkiye");
+        elements.postalCode.sendKeys("34582");
+        elements.next.click();
+
+        elements.phoneNumber.sendKeys("050515154855");
+        elements.next.click();
+
+        elements.relationship_type.click();
+        Select relationship = new Select(elements.relationship_type);
+        relationship.selectByIndex(5);
+        elements.person_Name.sendKeys("Gencer");
+        elements.next.click();
+
+        elements.submit.click();
+        Tools.Bekle(5);
+        System.out.println("Soyad Kontrol Ediliyor... : "+elements.alert1.getText());
+        Assert. assertTrue(elements.alert1.getText().contains("Genç"));
+        String IDFull = myElements.getID.getText();
+        ID2 = IDFull.substring(10);
+
+        System.out.println("ID = " +ID2 );
+
+    }
+
+    @Test
+    public void PatientRegistration3() throws AWTException {
+        TC408_409_Elments myElements = new TC408_409_Elments();
+        myElements.goHomePage.click();
+        myElements.DataManagement.click();
+        myElements.MergePatients.click();
+
+        myElements.Patient1.sendKeys(ID1);
+        myElements.Patient2.sendKeys(ID2);
+        Robot robot = new Robot();
+        for (int i = 0; i < 2; i++) {
+            robot.keyPress(KeyEvent.VK_TAB);
+            robot.keyRelease(KeyEvent.VK_TAB);
+        }
+        wait.until(ExpectedConditions.elementToBeClickable(myElements.mergeConfirm));
+        myElements.mergeConfirm.click();
+        wait.until(ExpectedConditions.elementToBeClickable(myElements.selectPatient));
+        myElements.selectPatient.click();
+        wait.until(ExpectedConditions.visibilityOfAllElements(myElements.mergeConfirm));
+        myElements.mergeConfirm.click();
+        System.out.println(myElements.isMerge.getText());
+        String Patient1_2="Patient ID"+ID1+ID2;
+        Assert.assertEquals(Patient1_2,myElements.isMerge.getText(),"Patient ID"+ID1+" "+ID2);
+
+    }
+
 }
